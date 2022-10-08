c2db659ada91edd9eb7f50824b4f7e582cf1f7b0
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2022-10-08 18:18:31 +0000

    Add functions to script

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongScore() {
+
+}
+
+function displayButtons() {
+
+}
+
+function displayFlag() {
+
+}
