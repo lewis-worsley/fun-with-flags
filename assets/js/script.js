1751cbeca751912e716fe548fc7f54a3f07b8ffa
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2022-10-08 23:26:27 +0000

    Some CSS styling added and const on JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
-
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === "option1") {
-                alert("You clicked submit");
-            } else {
-                let game = this.getAttribute("data-type");
-                alert(`You clicked ${game}`);
-            }
-        })
-    }
-})
-
-
-function runGame() {
-
-}
-
-function checkAnswer() {
-
-}
-
-function calculateCorrectAnswer() {
-
-}
-
-function incrementScore() {
-
-}
-
-function incrementWrongScore() {
-
-}
-
-function displayButtons() {
-
-}
-
-function displayFlag() {
-
-}
+const displayFlag = document.getElementById("flagImage");
+const firstOption = document.getElementById("option1");
+const secondOption = document.getElementById("option2");
+const ThirdOption = document.getElementById("option3");
+const playerCorrectScore = document.getElementById("correct");
+const playerWrongScore = document.getElementById("incorrect");
