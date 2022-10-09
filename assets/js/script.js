544c351f4e84e229ee1e358f3868a1d5efbaae13
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2022-10-09 10:17:28 +0000

    Add array of flags to JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,209 @@
+const flags = [
+
+    {
+        name: 'Argentina',
+        fileName: 'argentina.png',
+        correctAnswer: 'Argentina',
+        options: [
+            'Argentina',
+            'Brazil',
+            'Egypt'
+        ]
+    },
+    {
+        name: 'Brazil',
+        fileName: 'brazil.png',
+        correctAnswer: 'Brazil',
+        options: [
+            'England',
+            'Brazil',
+            'Chile'
+        ]
+    },
+    {
+        name: 'Cambodia',
+        fileName: 'cambodia.png',
+        correctAnswer: 'Cambodia',
+        options: [
+            'Vietnam',
+            'Laos',
+            'Cambodia'
+        ]
+    },
+    {
+        name: 'Cameroon',
+        fileName: 'cameroon.png',
+        correctAnswer: 'Cameroon',
+        options: [
+            'Senegal',
+            'Cameroon',
+            'New Zealand'
+        ]
+    },
+    {
+        name: 'Croatia',
+        fileName: 'croatia.png',
+        correctAnswer: 'Croatia',
+        options: [
+            'Croatia',
+            'Montenegro',
+            'Norway'
+        ]
+    },
+    {
+        name: 'Cuba',
+        fileName: 'cuba.png',
+        correctAnswer: 'Cuba',
+        options: [
+            'Barbados',
+            'Jamaica',
+            'Cuba'
+        ]
+    },
+    {
+        name: 'Egypt',
+        fileName: 'egypt.png',
+        correctAnswer: 'Egypt',
+        options: [
+            'Egypt',
+            'Israel',
+            'Italy'
+        ]
+    },
+    {
+        name: 'England',
+        fileName: 'england.png',
+        correctAnswer: 'England',
+        options: [
+            'England',
+            'Finland',
+            'Scotland'
+        ]
+    },
+    {
+        name: 'Israel',
+        fileName: 'israel.png',
+        correctAnswer: 'Israel',
+        options: [
+            'Qatar',
+            'Israel',
+            'Tunisia'
+        ]
+    },
+    {
+        name: 'Jamaica',
+        fileName: 'jamaica.png',
+        correctAnswer: 'Jamaica',
+        options: [
+            'South Africa',
+            'USA',
+            'Jamaica'
+        ]
+    },
+    {
+        name: 'Japan',
+        fileName: 'japan.png',
+        correctAnswer: 'Japan',
+        options: [
+            'Japan',
+            'China',
+            'South Korea'
+        ]
+    },
+    {
+        name: 'Mexico',
+        fileName: 'mexico.png',
+        correctAnswer: 'Mexico',
+        options: [
+            'Mexico',
+            'Costa Rica',
+            'Colombia'
+        ]
+    },
+    {
+        name: 'Morocco',
+        fileName: 'morocco.png',
+        correctAnswer: 'Morocco',
+        options: [
+            'UAE',
+            'Saudi Arabia',
+            'Morocco'
+        ]
+    },
+    {
+        name: 'Namibia',
+        fileName: 'namibia.png',
+        correctAnswer: 'Namibia',
+        options: [
+            'Nigeria',
+            'Namibia',
+            'Ghana'
+        ]
+    },
+    {
+        name: 'Norway',
+        fileName: 'norway.png',
+        correctAnswer: 'Norway',
+        options: [
+            'Sweden',
+            'Ireland',
+            'Norway'
+        ]
+    },
+    {
+        name: 'Singapore',
+        fileName: 'singapore.png',
+        correctAnswer: 'Singapore',
+        options: [
+            'Singapore',
+            'Malaysia',
+            'Fuji'
+        ]
+    },
+    {
+        name: 'Spain',
+        fileName: 'spain.png',
+        correctAnswer: 'Spain',
+        options: [
+            'Singapore',
+            'Malaysia',
+            'Spain'
+        ]
+    },
+    {
+        name: 'Switzerland',
+        fileName: 'switzerland.png',
+        correctAnswer: 'Switzerland',
+        options: [
+            'Greece',
+            'Portugal',
+            'Switzerland'
+        ]
+    },
+    {
+        name: 'Thailand',
+        fileName: 'thailand.png',
+        correctAnswer: 'Thailand',
+        options: [
+            'Indonesia',
+            'Australia',
+            'Thailand'
+        ]
+    },
+    {
+        name: 'Tunisia',
+        fileName: 'tunisia.png',
+        correctAnswer: 'Tunisia',
+        options: [
+            'Turkey',
+            'Tunisia',
+            'Kenya'
+        ]
+    },
+
+];
+
+console.log(flags);
 const displayFlag = document.getElementById("flagImage");
 const firstOption = document.getElementById("option1");
 const secondOption = document.getElementById("option2");
