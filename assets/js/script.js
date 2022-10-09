d3daff8f1b5db05eddc1199d7f3cb4de4e8bbc29
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2022-10-09 11:25:49 +0000

    Add score functions for game

--- assets/js/script.js
+++ assets/js/script.js
@@ -209,9 +209,10 @@ const displayFlag = document.querySelector(".js-flag-img");
 const firstOption = document.querySelector(".js-option1");
 const secondOption = document.querySelector(".js-option2");
 const thirdOption = document.querySelector(".js-option3");
-const playerCorrectScore = document.querySelector(".js-correct");
-const playerWrongScore = document.querySelector(".js-incorrect");
 
+let playerCorrectScore = document.querySelector(".js-correct");
+let playerWrongScore = document.querySelector(".js-incorrect");
+let roundsPlayed = document.querySelector(".js-round-played");
 let currentCorrectAnswer = '';
 
 const updateQuiz = () => {
@@ -225,34 +226,55 @@ const updateQuiz = () => {
     console.log(currentCorrectAnswer);
 }
 
+const incrementRound = () => {
+    roundsPlayed.innerHTML = parseInt(roundsPlayed.innerHTML) + 1;
+}
+
+const incrementCorrectScore = () => {
+    playerCorrectScore.innerHTML = parseInt(playerCorrectScore.innerHTML) + 1;
+}
+
+const incrementWrongScore = () => {
+    playerWrongScore.innerHTML = parseInt(playerWrongScore.innerHTML) + 1;
+}
+
 firstOption.addEventListener('click', () => {
     const option = firstOption.innerHTML;
     if (option === currentCorrectAnswer) {
         alert('You are right!!!')
+        incrementCorrectScore();
     } else {
         alert(`You selected ${option} which is wrong!!! The correct answer is ${currentCorrectAnswer}`)
+        incrementWrongScore();
     }
     updateQuiz();
+    incrementRound();
 });
 
 secondOption.addEventListener('click', () => {
     const option = secondOption.innerHTML;
     if (option === currentCorrectAnswer) {
         alert('You are right!!!')
+        incrementCorrectScore();
     } else {
         alert(`You selected ${option} which is wrong!!! The correct answer is ${currentCorrectAnswer}`)
+        incrementWrongScore();
     }
     updateQuiz();
+    incrementRound();
 });
 
 thirdOption.addEventListener('click', () => {
     const option = thirdOption.innerHTML;
     if (option === currentCorrectAnswer) {
         alert('You are right!!!')
+        incrementCorrectScore();
     } else {
         alert(`You selected ${option} which is wrong!!! The correct answer is ${currentCorrectAnswer}`)
+        incrementWrongScore();
     }
     updateQuiz();
+    incrementRound();
 });
 
 updateQuiz();
